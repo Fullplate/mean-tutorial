d7e4a6b78a9a0efa191e7c8af9ec0db136b2e275
Michael Harmer <michaelharmer.au@gmail.com>
2015-02-01 14:06:44 +1100

    Hooked up Angular frontend to backend API. [TESTED]

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -8,90 +8,127 @@ angular.module('flapperNews', ['ui.router'])
                 .state('home', {
                     url: '/home',
                     templateUrl: '/home.html',
-                    controller: 'MainCtrl'
+                    controller: 'MainCtrl',
+                    // resolve: before 'home' state is entered, use 'posts' factory's .getAll() method
+                    // results are located in the controller's injected/shared 'posts' object
+                    // https://github.com/angular-ui/ui-router/wiki#resolve
+                    resolve: {
+                        postPromise: ['posts', function(posts) {
+                            return posts.getAll();
+                        }]
+                    }
                 })
                 .state('posts', {
                     url: '/posts/{id}',
                     templateUrl: '/posts.html',
-                    controller: 'PostsCtrl'
+                    controller: 'PostsCtrl',
+                    resolve: {
+                        // retrieve 'post' object for controller before rendering /posts/{id}
+                        post: ['$stateParams', 'posts', function($stateParams, posts) {
+                            return posts.get($stateParams.id);
+                        }]
+                    }
                 });
 
             $urlRouterProvider.otherwise('home');
         }
     ])
-    .factory('posts', [function() {
-        // export an object from factory that has an array property "posts"
-        // this allows us to add extra properties onto o in the future
+    .factory('posts', ['$http', function($http) {
+        // return object 'o', with 'posts' array and a method to update it
         var o = {
             posts: []
         };
+
+        /* GET /posts */
+        o.getAll = function() {
+            return $http.get('/posts').success(function(data) {
+                angular.copy(data, o.posts); // .copy function updates ui appropriately
+            });
+        };
+
+        /* POST /posts */
+        o.create = function(post) {
+            return $http.post('/posts', post).success(function(data) {
+                o.posts.push(data);
+            });
+        };
+
+        /* PUT /posts/:post/upvote */
+        o.upvote = function(post) {
+            return $http.put('/posts/' + post._id + '/upvote')
+                .success(function(data) {
+                    post.upvotes += 1; // use techniques like this so we don't retrieve entire post again
+                });
+        };
+
+        /* GET /posts/:post */
+        o.get = function(id) {
+            // using .then(function...) creates a promise
+            return $http.get('/posts/' + id).then(function(res) {
+                return res.data;
+            });
+        };
+
+        /* POST /posts/:post/comments */
+        o.addComment = function(id, comment) {
+            return $http.post('/posts/' + id + "/comments", comment);
+        };
+
+        /* PUT /posts/:post/comments/:comment/upvote */
+        o.upvoteComment = function(post, comment) {
+            return $http.put('/posts/' + post._id + "/comments/" + comment._id + '/upvote')
+                .success(function(data) {
+                    comment.upvotes += 1;
+                });
+        };
+
         return o;
     }])
-    .controller('MainCtrl', [
+    .controller('MainCtrl', [ // defined with name, [injected object, ..., handler function(injected object, ...)]
         '$scope',
         'posts',
         function($scope, posts){
-            // this is a sort of controller + model
-            // it contains data (eg model) and methods to manipulate data (eg controller)
-            // and then the template/view in the html takes the methods and builds the view
-            // and everything is nicely linked up...
-
             $scope.posts = posts.posts; // bind posts to $scope
                                         // any changes to $scope.posts will be reflected in the service
                                         // and visible to other controllers using the service.
 
-            // make sure to push, and not re-assign the array like I was doing...
-            // keep it by reference so all controllers work on the same data
-            $scope.posts.push(
-                {
-                    title: 'Awesome post!',
-                    upvotes: 5,
-                    link: 'http://google.com',
-                    comments: [
-                        {author: 'Michael', body: 'Wow so cool!', upvotes: 2},
-                        {author: 'Henry', body: 'What gr8 names', upvotes: 5}
-                    ]
-                }
-            );
-
+            // adding functions to $scope will make them accessible through our html view
             $scope.addPost = function() {
                 if (!$scope.title || $scope.title === '') { return; }
-                $scope.posts.push({
+                posts.create({
                     title: $scope.title,
-                    link: $scope.link,
-                    upvotes: 0,
-                    comments: [
-                        {author: 'Michael', body: 'Wow so cool!', upvotes: 2},
-                        {author: 'Henry', body: 'What gr8 names', upvotes: 5}
-                    ]
+                    link: $scope.link
                 });
                 $scope.title = '';
                 $scope.link = '';
             };
 
             $scope.incrementUpvotes = function(post) {
-                post.upvotes += 1;
+                posts.upvote(post);
             };
         }])
     .controller('PostsCtrl', [
         '$scope',
-        '$stateParams',
         'posts',
-        function($scope, $stateParams, posts) {
-            // add post to our 2-way-data-binding object ($scope)
-            // retrieve it through the posts factory, via an id accessible through $stateParams
-            $scope.post = posts.posts[$stateParams.id]
+        'post', // post is injected via the 'resolve' in our url routing
+        function($scope, posts, post) {
+            $scope.post = post; // add post to our 2-way-binding so it's accessible in view
+                                // and any changes we make to it appear immediately
 
             $scope.addComment = function() {
                 if ($scope.body === '') { return; }
-                console.log("hello: "+$scope.post);
-                $scope.post.comments.push({
+                posts.addComment(post._id, {
                     body: $scope.body,
-                    author: 'user',
-                    upvotes: 0
+                    author: 'user'
+                }).success(function(comment) {
+                    $scope.post.comments.push(comment);
                 });
                 $scope.body = '';
-            }
+            };
+
+            $scope.incrementUpvotes = function(comment) {
+                posts.upvoteComment(post, comment);
+            };
         }
     ]);
 
